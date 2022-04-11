57006c5c2864550ab9ec3a4cb5541a119c1198f2
Sergei Khramtsov <vocmarh1989@gmail.com>
2022-04-11 09:41:38 +0500

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Напишите что-то");
+        var modif = Console.ReadLine();
+
+        Console.WriteLine("Укажите глубину эха");
+        var deep = int.Parse(Console.ReadLine());
+
+        Echo(modif, deep);
+
+        Console.ReadKey();
+    }
+
+    static void Echo(string saidworld, int deep)
+    {
+        var modif = saidworld;
+        if (modif.Length > 2)
+        {
+            modif = modif.Remove(0, 2);
+        }    
+        Console.WriteLine("..." + modif);
+
+        if (deep > 1)
+        {
+            Echo(modif, deep - 1);
+        }
+    }
+}
